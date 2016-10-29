3eed8f3b2585c5b0283b5ccbefa31367cd2036dd
Daniel Lim <hbt@markhamboards.com>
2016-10-29 16:18:32 -0400

    update progress
    progressing

--- src/js/actions/AppActions.js
+++ src/js/actions/AppActions.js
@@ -2,6 +2,15 @@ var AppDispatcher = require('../dispatcher/AppDispatcher');
 var AppConstants = require('../constants/AppConstants');
 
 var AppActions = {
+
+	saveContact: function(contact){
+		console.log(contact);
+		AppDispatcher.handleViewAction({
+			actionType: AppConstants.SAVE_CONTACT,
+			contact: contact
+		});
+	},
+
 	receiveItems: function(items){
 		AppDispatcher.handleViewAction({
 			actionType: AppConstants.RECEIVE_ITEMS,
