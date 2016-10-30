640941624be3499a8461f08b522af279e104d6e7
Daniel Lim <hbt@markhamboards.com>
2016-10-30 15:51:49 -0400

    re mods
    re mods

--- src/js/utils/appAPI.js
+++ src/js/utils/appAPI.js
@@ -12,6 +12,55 @@ var config = {
 firebase.initializeApp(config);
 
 
+// Get Elements
+    const txtEmail 		= document.getElementById("txtEmail");
+    const txtPassword 	= document.getElementById("txtPassword");
+    const btnLogin 		= document.getElementById("btnLogin");
+    const btnSignUp 	= document.getElementById("btnSignUp");
+    const btnLogOut 	= document.getElementById("btnLogout");
+
+// Add Log-in Event
+    btnLogin.addEventListener('click', e => {
+		// Get Email & Password
+        const email 	= txtEmail.value;
+        const pass  	= txtPassword.value;
+        const auth  	= firebase.auth();
+		// Sign-in
+        const promise 	= auth.signInWithEmailAndPassword(email, pass);
+        promise.catch(e => console.log(e.message));
+	});
+
+// Add Sign-in Event
+    btnSignUp.addEventListener('click', e => {
+		// Get Email & Password
+		// TODO: Check for Real Emails
+
+        const email 	= txtEmail.value;
+        const pass  	= txtPassword.value;
+        const auth  	= firebase.auth();
+		// Sign-in
+        const promise 	= auth.createUserWithEmailAndPassword(email, pass);
+        promise.catch(e => console.log(e.message));
+	});	
+
+// Add Real-Time Autho. Listener
+	firebase.auth().onAuthStateChanged(firebaseUser => {
+
+		// Handle Errors here.
+        //	let errorCode = error.code;
+		// 	let errorMessage = error.message;
+        // [START_EXCLUDE]
+
+		// firebaseUser will be null if not logged in
+		if (firebaseUser) {
+			console.log(firebaseUser);
+		} else {
+//			alert(errorMessage);
+			console.log('Not Logged In!');
+		}
+		console.log('firebaseUser: ', firebaseUser); // firebase.User , email
+	});
+
 
 module.exports = {
 	saveContact: function(contact) {
