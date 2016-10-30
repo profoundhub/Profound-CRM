2e4ff5e1e2eb86148c82fefe22be278f860a6d1e
Daniel Lim <hbt@markhamboards.com>
2016-10-30 15:38:33 -0400

    Update Since
    Update Since

--- src/js/utils/appAPI.js
+++ src/js/utils/appAPI.js
@@ -1,20 +1,67 @@
 let Firebase = require('firebase');
 let AppActions = require('../actions/AppActions');
 
+// Initialize Firebase
 var config = {
     apiKey: "AIzaSyBnEzTxUQ7PzL27P77DYE-UX-kc8MFkNT4",
     authDomain: "profoundcrm.firebaseapp.com",
-    databaseURL: "https://profoundcrm.firebaseio.com",            
+    databaseURL: "https://profoundcrm.firebaseio.com",           
+	storageBucket: "profoundcrm.appspot.com",
+    messagingSenderId: "235081852006" 
 };
-
 firebase.initializeApp(config);
 
-	// Get Elements
-    const txtEmail = document.getElementById("txtEmail");
-    const txtPassword = document.getElementById("txtPassword");
-    const btnLogin = document.getElementById("btnLogin");
-    const btnSignUp = document.getElementById("btnSignUp");
-    const btnLogOut = document.getElementById("btnLogOut");
+
+
+// Get Elements
+    const txtEmail 		= document.getElementById("txtEmail");
+    const txtPassword 	= document.getElementById("txtPassword");
+    const btnLogin 		= document.getElementById("btnLogin");
+    const btnSignUp 	= document.getElementById("btnSignUp");
+    const btnLogOut 	= document.getElementById("btnLogout");
+
+// Add Log-in Event
+    btnLogin.addEventListener('click', e => {
+		// Get Email & Password
+        const email 	= txtEmail.value;
+        const password  = txtPassword.value;
+        const auth  	= firebase.auth();
+		// Sign-in
+        const promise 	= auth.signInWithEmailAndPassword(email, password);
+        promise.catch(e => console.log(e.message));
+	});
+
+// Add Sign-in Event
+    btnSignUp.addEventListener('click', e => {
+		// Get Email & Password
+		// TODO: Check for Real Emails
+
+        const email 	= txtEmail.value;
+        const password  = txtPassword.value;
+        const auth  	= firebase.auth();
+		// Sign-in
+        const promise 	= auth.createUserWithEmailAndPassword(email, password);
+        promise.catch(e => console.log(e.message));
+	});	
+
+// Add Real-Time Autho. Listener
+	firebase.auth().onAuthStateChanged(firebaseUser => {
+
+		// Handle Errors here.
+        //	let errorCode = error.code;
+		// 	let errorMessage = error.message;
+        // [START_EXCLUDE]
+
+		// firebaseUser will be null if not logged in
+		if (firebaseUser) {
+			console.log(firebaseUser);
+		} else {
+//			alert(errorMessage);
+			console.log('Not Logged In!');
+		}
+		console.log('firebaseUser: ', firebaseUser); // firebase.User
+	});
+
 
 module.exports = {
 	saveContact: function(contact) {
