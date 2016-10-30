3f0a6921995ae019867ba3321f401db35930f929
Daniel Lim <hbt@markhamboards.com>
2016-10-29 21:08:45 -0400

    reset Firebase
    reset Firebase configs Switch

--- src/js/utils/appAPI.js
+++ src/js/utils/appAPI.js
@@ -1,6 +1,18 @@
 let Firebase = require('firebase');
 let AppActions = require('../actions/AppActions');
 
+require("firebase/auth");
+require("firebase/database");
+
+
+// Leave out Storage
+//require("firebase/storage");
+
+var config = {
+  // ...
+};
+firebase.initializeApp(config);
+
 module.exports = {
 	saveContact: function(contact) {
 		this.firebaseRef = new Firebase('https://profoundcrm.firebaseio.com/profoundcrm');
